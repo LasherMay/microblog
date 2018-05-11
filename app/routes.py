2b8c3e2f807ccca695d5090bbfe7538b430ce27d
LasherMay <vanilladig@gmail.com>
2018-05-11 17:12:42 +0300

    Added login form, added Flask-WTF, modified base.html for using forms and messages from flash()

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+
 def index():
 	user = {'username': 'Andrew'}
 	posts = [
@@ -15,12 +17,19 @@ def index():
 		'body': 'The Avengers movie pssed me off'
 	},
         {
-                'author': {'username': 'Susan'},
-                'body': 'The Avengers movie pssed me off'
-        },
-        {
-                'author': {'username': 'Susan'},
-                'body': 'The Avengers movie pssed me off'
+                'author': {'username': 'Troll'},
+                'body': 'The Avengers movie is the best'
         }
 ]
 	return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title='Sign in', form=form)
