ad86047b5580990b3f8d13fe2f2d27aa18706f16
LasherMay <vanilladig@gmail.com>
2018-05-11 15:24:07 +0300

    Added Jinja2 render template to routes.py and additional info to template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	user = {'username': 'Andrew'}
+	posts = [
+	{
+		'author': {'username': 'John'},
+		'body': 'Beautiful day in Moscow!'
+	},
+	{
+		'author': {'username': 'Susan'},
+		'body': 'The Avengers movie pssed me off'
+	},
+        {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie pssed me off'
+        },
+        {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie pssed me off'
+        }
+]
+	return render_template('index.html', title='Home', user=user, posts=posts)
