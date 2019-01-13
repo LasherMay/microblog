4cbabe142fcebeee8ad17de2eb3b56e31b9809dd
LasherMay <vanilladig@gmail.com>
2019-01-13 22:54:00 +0300

    2nd lesson

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,43 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template
 from app import app
-from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
 	user = {'username': 'Andrew'}
-	posts = [
-	{
-		'author': {'username': 'John'},
-		'body': 'Beautiful day in Moscow!'
-	},
-	{
-		'author': {'username': 'Susan'},
-		'body': 'The Avengers movie pssed me off'
-	},
-        {
-                'author': {'username': 'Troll'},
-                'body': 'The Avengers movie is the best'
-        }
-]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+
+# 	posts = [
+# 	{
+# 		'author': {'username': 'John'},
+# 		'body': 'Beautiful day in Moscow!'
+# 	},
+# 	{
+# 		'author': {'username': 'Susan'},
+# 		'body': 'The Avengers movie pssed me off'
+# 	},
+#         {
+#                 'author': {'username': 'Troll'},
+#                 'body': 'The Avengers movie is the best'
+#         }
+# ]
+	return render_template('index.html', title='Home', user=user)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+# from flask import render_template, flash, redirect, url_for
 
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-			form.username.data, form.remember_me.data))
-		return redirect(url_for('index'))
-	return render_template('login.html', title='Sign in', form=form)
+# from app.forms import LoginForm
+#
+
+#
+
+#
+#
+# @app.route('/login', methods=['GET', 'POST'])
+#
+# def login():
+# 	form = LoginForm()
+# 	if form.validate_on_submit():
+# 		flash('Login requested for user {}, remember_me={}'.format(
+# 			form.username.data, form.remember_me.data))
+# 		return redirect(url_for('index'))
+# 	return render_template('login.html', title='Sign in', form=form)
